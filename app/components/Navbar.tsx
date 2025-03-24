08e85d3c19c7ce40894e28fbf0028d9b385ec33e
tunglam16402 <lamnguyen16042002@gmail.com>
2025-03-24 23:01:19 +0700

    login with authjs

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,46 @@
+import { auth, signOut, signIn } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30}></Image>
         </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({redirectTo:"/"});
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button>Login</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
