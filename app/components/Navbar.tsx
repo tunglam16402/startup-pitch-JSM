05b5b8f0c0f5d1a71241e8b2c24d1ac5a58e4bb6
tunglam16402 <lamnguyen16042002@gmail.com>
2025-03-28 14:59:30 +0700

    font and theme

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = async () => {
               >
                 <button type="submit">Logout</button>
               </form>
-              <Link href={`/user/${session.id}`}>
+              <Link href={`/user/${session}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
